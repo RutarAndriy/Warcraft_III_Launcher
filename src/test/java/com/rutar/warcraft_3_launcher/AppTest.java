f25f1bcdbb4057a29bd4e0773bef24aa0638f22a
RutarAndriy <RutarAndriy@gmail.com>
2021-12-17 12:24:54 +0200

    Generated project structure

---
+++ src/test/java/com/rutar/warcraft_3_launcher/AppTest.java
@@ -0,0 +1,19 @@
+package com.rutar.warcraft_3_launcher;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+// ............................................................................
+
+public class AppTest {
+
+///////////////////////////////////////////////////////////////////////////////
+
+@Test
+public void should_Answer_With_True()
+    { assertTrue(true); }
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
